a93fbe977e597f85b52532fcc79033da0ec4c8d7
Jason Christopher <jchristopher2448@gmail.com>
2023-01-12 12:48:07 -0600

    finished Lab 19

--- automation.py
+++ automation.py
@@ -8,12 +8,19 @@ def find_contact_info():
     file.close()
 
     phone_seq = r"([+]?[0-9]{1,3}[-| |.|])?[(]?([0-9]{3})[)]?[-| |.]?([0-9]{3})[-| |.]?([0-9]{4})(x[0-9]{1,5})?"
+    email_seq = r"(\w*@\w*[-]?\w*(\.net|\.com|\.org|\.info|\.biz))"
     phone_match = re.findall(phone_seq, content)
+    email_match = re.findall(email_seq, content)
 
     with open('collected/phone_numbers.txt', 'w') as f:
         for match in phone_match:
             phone_num = str(match[1]) + "-" + str(match[2]) + "-" + str(match[3]) + str(match[4])
             f.write(phone_num + "\n")
+
+    with open('collected/emails.txt', 'w') as f:
+        for match in email_match:
+            email = str(match[0])
+            f.write(email + "\n")
 
 
 if __name__ == "__main__":
