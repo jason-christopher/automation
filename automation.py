3d1f2043519b1893e4801ab141cc8b595d7bd3f6
Jason Christopher <jchristopher2448@gmail.com>
2023-01-12 10:12:56 -0600

    grabs all phone numbers and writes each to file

--- automation.py
+++ automation.py
@@ -1,6 +1,19 @@
-file = open('assets/potential_contacts.txt', 'r')
-content = file.read()
-file.close()
+import re
+import shutil
 
-with open('collected/emails.txt', 'w') as f:
-    f.write('hello world')
+
+def find_contact_info():
+    file = open('assets/potential_contacts.txt', 'r')
+    content = file.read()
+    file.close()
+
+    phone_seq = "([+]?[0-9]{1,3}[-| |.|])?([(][0-9]{3}[)]|[0-9]{3}[-| |.|])?([0-9]{3}[-| |.|][0-9]{4})(x[0-9]{1,5})*"
+    phone_match = re.findall(phone_seq, content)
+
+    with open('collected/phone_numbers.txt', 'w') as f:
+        for match in phone_match:
+            f.write("".join(match) + "\n")
+
+
+if __name__ == "__main__":
+    find_contact_info()
